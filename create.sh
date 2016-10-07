176a953463de2e25c73b19b7629cf461ed94b5bf
Federico Gimenez <fgimenez@coit.es>
2016-10-07 09:35:47 +0200

    channel parameter

--- create.sh
+++ create.sh
@@ -3,12 +3,13 @@ set -x
 
 MODELS_DIR="$HOME/Desktop/validate_image/models"
 IMAGES_DIR="./images"
+CHANNEL=${1:-edge}
 
 create(){
     rm -rf "$IMAGES_DIR" && mkdir -p "$IMAGES_DIR"
     for platform in pc pi3 pi2 dragonboard; do
         output="${IMAGES_DIR}/${platform}-edge.img"
-        sudo /snap/bin/ubuntu-image -c edge -o "$output" "$MODELS_DIR/${platform}.model"
+        sudo /snap/bin/ubuntu-image -c "$CHANNEL" -o "$output" "$MODELS_DIR/${platform}.model"
     done
 }
 
