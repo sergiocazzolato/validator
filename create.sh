674dc13e7e724815f0d8a9739869d85c7512a77b
Federico Gimenez <fgimenez@coit.es>
2016-10-11 09:17:32 +0200

    output file name depends on channel

--- create.sh
+++ create.sh
@@ -8,7 +8,7 @@ CHANNEL=${1:-edge}
 create(){
     rm -rf "$IMAGES_DIR" && mkdir -p "$IMAGES_DIR"
     for platform in pc pi3 pi2 dragonboard; do
-        output="${IMAGES_DIR}/${platform}-edge.img"
+        output="${IMAGES_DIR}/${platform}-${CHANNEL}.img"
         sudo /snap/bin/ubuntu-image -c "$CHANNEL" -o "$output" "$MODELS_DIR/${platform}.model"
     done
 }
