5acf4e989c1f8e5620251da8a50a43b416f72351
Federico Gimenez <fgimenez@canonical.com>
2016-10-18 22:33:05 +0200

    changes for new ubuntu-image

--- create.sh
+++ create.sh
@@ -8,8 +8,13 @@ CHANNEL=${1:-edge}
 create(){
     rm -rf "$IMAGES_DIR" && mkdir -p "$IMAGES_DIR"
     for platform in pc pi3 pi2 dragonboard; do
+        if [ "$platform" = "pc" ]; then
+            image_option="--image-size 3G"
+        else
+            image_option=""
+        fi
         output="${IMAGES_DIR}/${platform}-${CHANNEL}.img"
-        sudo /snap/bin/ubuntu-image -c "$CHANNEL" -o "$output" "$MODELS_DIR/${platform}.model"
+        sudo /snap/bin/ubuntu-image "$image_option" -c "$CHANNEL" -o "$output" "$MODELS_DIR/${platform}.model"
     done
 }
 
