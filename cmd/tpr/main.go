fb06b6b38007d6bb9725318a72994f46342aaf0b
Federico Gimenez <fgimenez@canonical.com>
2017-04-12 20:12:05 +0200

    body commented for now

--- cmd/tpr/main.go
+++ cmd/tpr/main.go
@@ -1,25 +1,19 @@
 package main
 
-import (
-	"log"
-
-	"github.com/fgimenez/validator/pkg/flags"
-	"github.com/fgimenez/validator/pkg/runner"
-	"github.com/fgimenez/validator/pkg/types"
-)
-
 func main() {
-	options := flags.Parse()
+	/*
+		options := flags.Parse()
 
-	deps := &types.RunnerDependencies{
-		C:  cli.New(),
-		T:  testflinger.New(),
-		Sd: systemd.New(),
-		Sp: splitter.New(),
-	}
-	runner := runner.New(deps)
+		deps := &types.RunnerDependencies{
+			C:  cli.New(),
+			T:  testflinger.New(),
+			Sd: systemd.New(),
+			Sp: splitter.New(),
+		}
+		runner := runner.New(deps)
 
-	if err := runner.Run(options); err != nil {
-		log.Fatalf(err)
-	}
+		if err := runner.Run(options); err != nil {
+			log.Fatalf(err)
+		}
+	*/
 }
