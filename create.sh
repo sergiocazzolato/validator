8488482e400e43abb69cc3b4723ae1c5884bd3eb
Federico Gimenez <fgimenez@canonical.com>
2017-07-03 14:18:00 +0200

    do not use stable kernel (it is refreshed anyway to the target channel upon first boot)

--- create.sh
+++ create.sh
@@ -5,22 +5,6 @@ CHANNEL=${1:-edge}
 PLATFORMS=${2:-"dragonboard pc-amd64 pc-i386 pi3 pi2"}
 
 for platform in $PLATFORMS; do
-    if [[ "$platform" == pi* ]]; then
-        kernel_snap_name="pi2-kernel"
-        arch="armhf"
-    elif [ "$platform" = dragonboard ]; then
-        arch="arm64"
-        kernel_snap_name="dragonboard-kernel"
-    elif [ "$platform" = pc-amd64 ]; then
-        arch="amd64"
-        kernel_snap_name="pc-kernel"
-    else
-        arch="i386"
-        kernel_snap_name="pc-kernel"
-    fi
-
-    UBUNTU_STORE_ARCH=$arch snap download $kernel_snap_name
-
     if [[ "$platform" == pc* ]]; then
         image_option="--image-size 3G"
     else
@@ -31,7 +15,5 @@ for platform in $PLATFORMS; do
     sudo /snap/bin/ubuntu-image "$image_option" \
          -c "$CHANNEL" \
          -O "$output" \
-         --extra-snaps ${kernel_snap_name}_*.snap \
          "./models/${platform}.model"
-    rm -f ${kernel_snap_name}_*.{snap,assert}
 done
