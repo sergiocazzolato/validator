b248042ce0cfd1a9b5da5e8accdc88918f28ef95
Sergio Cazzolato <sergiocazzolato@gmail.com>
2019-06-28 18:34:52 -0300

    New models to run nested vms

--- create-signed-models.sh
+++ create-signed-models.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-MODELS_PATH="./models"
+KEY=${1:-default}
+MODELS_PATH="./images/models"
 
 for model in $(find $MODELS_PATH/*.json); do
-    cat "$model" | snap sign -k default >"${model/-model.json/.model}" 2>&1
+    cat "$model" | snap sign -k "$KEY" >"${model/-model.json/.model}" 2>&1
 done
